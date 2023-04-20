1c5a16d83affe5d8bf3f23ce856d1cee233cf950
YAPO JEAN STEPHANE <71141123+Stephane1335@users.noreply.github.com>
2023-04-20 19:56:08 +0200

    Modif

---
+++ app/src/main/java/com/example/network_yc_/addNetwork.kt
@@ -0,0 +1,11 @@
+package com.example.network_yc_
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class addNetwork : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_network)
+    }
+}
