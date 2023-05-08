0efbd477191f1d131277a51124e84c5b0846b1ff
Coulibaly Othniel <coulibalyothniel@gmail.com>
2023-05-08 19:01:23 +0000

    Modification

--- app/src/main/java/com/example/network_yc_/MainActivity.kt
+++ app/src/main/java/com/example/network_yc_/MainActivity.kt
@@ -2,16 +2,18 @@ package com.example.network_yc_
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.network_yc_.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
+
 }
